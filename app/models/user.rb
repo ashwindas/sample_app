bef208d10b0a956e2d958b3ed9686586cabecab1
Ashwin Das <ashwin.das@sv.cmu.edu>
2011-09-25 18:07:57 -0400

    Done with sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-# == Schema Information
+  # == Schema Information
 #
 # Table name: users
 #
@@ -38,6 +38,11 @@ class User < ActiveRecord::Base
     user = find_by_email(email)
     return nil  if user.nil?
     return user if user.has_password?(submitted_password)
+  end
+
+  def self.authenticate_with_salt(id, cookie_salt)
+    user = find_by_id(id)
+    (user && user.salt == cookie_salt) ? user : nil
   end
 
   private
